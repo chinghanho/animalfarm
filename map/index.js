b41d2c2d827a0689ff9f3a0dc6f4054e88d83e44
CHH <chh@chh.tw>
2017-02-17 18:21:05 +0800

    輸出地圖的資料加入 doors 的資料

--- map/index.js
+++ map/index.js
@@ -4,12 +4,23 @@ let fs   = require('fs')
 let data = require('./tiled.json')
 let _    = require('lodash')
 
-let layersData = getLayerData()
-let json = combineLayers(layersData)
+let json = {}
+
+let layersData, doorsData
+
+layersData = getLayerData(data)
+layersData = combineLayers(layersData)
+
+doorsData = getDoorsData(data)
+doorsData = parseDoorsData(doorsData)
+
+json['data'] = layersData
+json['doors'] = doorsData
+
 output(json)
 
 
-function getLayerData() {
+function getLayerData(data) {
 
     return data['layers'].reduce((result, layer) => {
 
@@ -22,9 +33,9 @@ function getLayerData() {
 
 }
 
-function combineLayers(layersData) {
+function combineLayers(data) {
 
-    return _.zipWith(...layersData, function () {
+    return _.zipWith(...data, function () {
 
         let result = _.compact(arguments)
 
@@ -37,6 +48,37 @@ function combineLayers(layersData) {
         else {
             return result
         }
+    })
+
+}
+
+function getDoorsData(data) {
+
+    let result
+
+    data['layers'].forEach(function (layer) {
+
+        if (layer.name === 'doors') {
+            result = layer.objects
+        }
+
+    })
+
+    return result
+
+}
+
+function parseDoorsData(data) {
+
+    return data.map(function (object) {
+
+        return {
+            x: object.x,
+            y: object.y,
+            dx: object.properties.x,
+            dy: object.properties.y
+        }
+
     })
 
 }
