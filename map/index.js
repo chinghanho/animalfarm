8598d287c03a5ecfb81c6a5e6e830f9dcd7e7793
CHH <chh@chh.tw>
2017-02-17 17:51:47 +0800

    重構：重新整理輸出地圖的程式碼

--- map/index.js
+++ map/index.js
@@ -4,29 +4,53 @@ let fs   = require('fs')
 let data = require('./tiled.json')
 let _    = require('lodash')
 
-let layersData = data['layers'].map((layer) => layer.data)
+let layersData = getLayerData()
+let json = combineLayers(layersData)
+output(json)
 
-let result = _.zipWith(...layersData, function () {
 
-    let result = _.compact(arguments)
+function getLayerData() {
 
-    if (result.length === 0) {
-        return 0
-    }
-    else if (result.length === 1) {
-        return result[0]
-    }
-    else {
+    return data['layers'].reduce((result, layer) => {
+
+        if (layer['type'] === 'tilelayer') {
+            result.push(layer.data)
+        }
+
         return result
-    }
-})
+    }, [])
 
-fs.writeFile('world.json', JSON.stringify(result), function (err) {
+}
 
-    if (err) {
-        return console.error(err)
-    }
+function combineLayers(layersData) {
 
-    console.log('success write JSON file: world.json')
+    return _.zipWith(...layersData, function () {
 
-})
+        let result = _.compact(arguments)
+
+        if (result.length === 0) {
+            return 0
+        }
+        else if (result.length === 1) {
+            return result[0]
+        }
+        else {
+            return result
+        }
+    })
+
+}
+
+function output(json) {
+
+    fs.writeFile('world.json', JSON.stringify(json), function (err) {
+
+        if (err) {
+            return console.error(err)
+        }
+
+        console.log('success write JSON file: world.json')
+
+    })
+
+}
