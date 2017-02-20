70d82369c54a5f13621a045d5364393d95303a5d
CHH <chh@chh.tw>
2017-02-20 12:34:16 +0800

    修正 NPC 生成位置的計算

--- scripts/logic/npc_logic.js
+++ scripts/logic/npc_logic.js
@@ -12,8 +12,10 @@
 
         _onReady() {
             let npc = new Npc('oldman')
-            npc.setGridPosition(14, 8)
+            npc.setGridPosition(14, 28)
             npc.setSprite(this.game.sprites['oldman'], 'idle_down')
+            npc.x = (npc.gridX - this.game.camera.gridX) * this.game.map.tileSize
+            npc.y = (npc.gridY - this.game.camera.gridY) * this.game.map.tileSize
             npc.idleSpeed = 1000
             npc.idle()
             this.game.pathingGrid.register(npc)
