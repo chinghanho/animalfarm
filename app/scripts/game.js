11c6eb7ec1d7539748cd402403068a17f9294fb1
CHH <chh@chh.tw>
2017-03-07 11:01:17 +0800

    Game 初始化 layers 關注點分離

--- app/scripts/game.js
+++ app/scripts/game.js
@@ -6,11 +6,6 @@ class Game {
         this.camera = require('./camera')
         this.grid = require('./geometries/grid')
         this.layer = require('./geometries/layer')
-
-        this.container.appendChild(this.layer.create('div',    { id: 'bubbles' }))
-        this.container.appendChild(this.layer.create('canvas', { id: 'background' }))
-        this.container.appendChild(this.layer.create('canvas', { id: 'entities' }))
-        this.container.appendChild(this.layer.create('canvas', { id: 'foreground' }))
     }
 
     pointToGrid() {
@@ -19,6 +14,17 @@ class Game {
 
     gridToPoint() {
         //
+    }
+
+    initLayers() {
+        this.container.appendChild(this.layer.create('div',    { id: 'bubbles' }))
+        this.container.appendChild(this.layer.create('canvas', { id: 'background' }))
+        this.container.appendChild(this.layer.create('canvas', { id: 'entities' }))
+        this.container.appendChild(this.layer.create('canvas', { id: 'foreground' }))
+    }
+
+    start() {
+        this.initLayers()
     }
 
 }
