4ffa342ce4d43749ca4713677aba4c6c07f5b7d4
CHH <chh@chh.tw>
2017-03-14 14:37:58 +0800

    調整 animation class 模組

--- app/scripts/core/animation.js
+++ app/scripts/core/animation.js
@@ -1,74 +1,68 @@
-(function (root) {
+class Animation {
 
-    'use strict'
-
-    class Animation {
-
-        constructor(width, height, row, length) {
-            this.sprite = {
-                width:  width,
-                height: height,
-                row:    row,
-                length: length
-            }
-            this._counter = 0
-            this._animated = false
-            this.reset()
-            // show the first frame, prevent started with x:0, y:0
-            if (!this._animated) {
-                this.nextFrame()
-            }
+    constructor(width, height, row, length) {
+        this.sprite = {
+            width:  width,
+            height: height,
+            row:    row,
+            length: length
         }
-
-        extends(params) {
-            Object.assign(this.sprite, params)
-            return this
+        this._counter = 0
+        this._animated = false
+        this.reset()
+        // show the first frame, prevent started with x:0, y:0
+        if (!this._animated) {
+            this.nextFrame()
         }
-
-        setSpeed(speed) {
-            this.sprite.speed = speed
-        }
-
-        nextFrame() {
-            let index = this.currentFrame.index
-            index = (index < this.sprite.length - 1) ? index + 1 : 0
-
-            this.currentFrame.x = this.sprite.width * index
-            this.currentFrame.y = this.sprite.height * (this.sprite.row - 1)
-            this.currentFrame.index = index
-        }
-
-        isTimeToNextFrame(time) {
-            this._counter++
-            let times = 60 / (1000 / this.sprite.speed)
-            let result = (this._counter % (times / this.sprite.length)) < 1
-            return result
-        }
-
-        update(time) {
-            if (this.isTimeToNextFrame(time)) {
-                if (!this._animated) {
-                    this._animated = true
-                }
-                this.nextFrame()
-                return true
-            }
-            else {
-                return false
-            }
-        }
-
-        reset() {
-            this.currentFrame = {
-                index: 0,
-                x: 0,
-                y: 0
-            }
-            this._counter = 0
-        }
-
     }
 
-    root.Animation = Animation
+    extends(params) {
+        Object.assign(this.sprite, params)
+        return this
+    }
 
-})(this)
+    setSpeed(speed) {
+        this.sprite.speed = speed
+    }
+
+    nextFrame() {
+        let index = this.currentFrame.index
+        index = (index < this.sprite.length - 1) ? index + 1 : 0
+
+        this.currentFrame.x = this.sprite.width * index
+        this.currentFrame.y = this.sprite.height * (this.sprite.row - 1)
+        this.currentFrame.index = index
+    }
+
+    isTimeToNextFrame(time) {
+        this._counter++
+        let times = 60 / (1000 / this.sprite.speed)
+        let result = (this._counter % (times / this.sprite.length)) < 1
+        return result
+    }
+
+    update(time) {
+        if (this.isTimeToNextFrame(time)) {
+            if (!this._animated) {
+                this._animated = true
+            }
+            this.nextFrame()
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+    reset() {
+        this.currentFrame = {
+            index: 0,
+            x: 0,
+            y: 0
+        }
+        this._counter = 0
+    }
+
+}
+
+module.exports = Animation
